a99b5fdb6203fe66de1628da35a3ec385f7db3e2
Jon Smith <jon.smith@selectiveanalytics.com>
2018-12-06 12:10:01 +0000

    Various tidy ups

--- PermissionParts/Permissions.cs
+++ PermissionParts/Permissions.cs
@@ -11,30 +11,31 @@ namespace PermissionParts
         NotSet = 0, //error condition
 
         //Here is an example of very detailed control over something
-        [Display(GroupName = "Data", Name = "Read", Description = "can read data")]
+        [Display(GroupName = "Data", Name = "Read", Description = "Can read data")]
         DataRead = 0x10,
-        [Display(GroupName = "Data", Name = "Create", Description = "can create data")]
+        [Display(GroupName = "Data", Name = "Create", Description = "Can create data")]
         DataCreate = 0x11,
-        [Display(GroupName = "Data", Name = "Update", Description = "can update data")]
+        [Display(GroupName = "Data", Name = "Update", Description = "Can update data")]
         DataUpdate = 0x12,
-        [Display(GroupName = "Data", Name = "Delete", Description = "can delete data")]
+        [Display(GroupName = "Data", Name = "Delete", Description = "Can delete data")]
         DataDelete = 0x13,
 
-        [Display(GroupName = "UserAdmin", Name = "Read user", Description = "can list User")]
+        [Display(GroupName = "UserAdmin", Name = "Read users", Description = "Can list User")]
         UserRead = 0x20,
         //This is an example of grouping multiple actions under one permission
-        [Display(GroupName = "UserAdmin", Name = "Alter user", Description = "can do anything to the User")]
+        [Display(GroupName = "UserAdmin", Name = "Alter user", Description = "Can do anything to the User")]
         UserChange = 0x21,
 
-        [Display(GroupName = "UserAdmin", Name = "Read Role", Description = "can list Role")]
+        [Display(GroupName = "UserAdmin", Name = "Read Roles", Description = "Can list Role")]
         RoleRead = 0x28,
-        [Display(GroupName = "UserAdmin", Name = "Change Role", Description = "can create, update or delete a Role")]
+        [Display(GroupName = "UserAdmin", Name = "Change Role", Description = "Can create, update or delete a Role")]
         RoleChange = 0x29,
 
-        //This is an example of a permission that is linked to a option (paid for?) feature
-        //The code that turns roles to permissions can remove this permission if the user isn't allowed to access this feature
+        //This is an example of a permission linked to a optional (paid for?) feature
+        //The code that turns roles to permissions can
+        //remove this permission if the user isn't allowed to access this feature
         [PermissionLinkedToModule(Modules.Feature1)]
-        [Display(GroupName = "Feature1", Name = "Access", Description = "can allows a user to access feature1")]
+        [Display(GroupName = "Feature1", Name = "Access", Description = "Can allows a user to access feature1")]
         Feature1Access = 0x31,
 
         //This is an example of what to do with permission you don't used anymore.

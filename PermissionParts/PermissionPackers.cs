b44f03bd3bec3b382cd49c464bec13e970742521
Jon Smith <jon.smith@selectiveanalytics.com>
2018-12-09 14:31:08 +0000

    Updated header

--- PermissionParts/PermissionPackers.cs
+++ PermissionParts/PermissionPackers.cs
@@ -1,4 +1,5 @@
-﻿// Copyright (c) 2018 Inventory Innovations, Inc.
+﻿// Copyright (c) 2018 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
 
 using System;
 using System.Collections.Generic;

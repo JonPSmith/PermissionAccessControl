bcb91f28df89e310064f80d7e19cd8f4d1a50060
Jon Smith <jon.smith@selectiveanalytics.com>
2018-12-11 14:33:55 +0000

    Added ColorRoleController as example

---
+++ TestWebApp/Controllers/ColorRoleController.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) 2018 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TestWebApp.Controllers
+{
+    public class ColorRoleController : Controller
+    {
+        private static readonly List<string> MyData = new List<string> { "Red", "Blue", "Green", "Yellow" };
+
+        [Authorize(Roles = "Staff,Manager")]
+        public ActionResult Index()
+        {
+            return View(MyData);
+        }
+
+        [Authorize(Roles = "Manager")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Manager")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                MyData.Add(collection["Data"]);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [Authorize(Roles = "Manager")]
+        public ActionResult Delete(int id)
+        {
+            MyData.RemoveAt(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
